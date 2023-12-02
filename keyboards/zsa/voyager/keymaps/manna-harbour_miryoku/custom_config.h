3a7f361af3e2f1ebc5eed8f456a6a6b850779241
David Steinberger <mail.steinberger@gmail.com>
2023-12-02 23:45:00 +0100

    initial

--- keyboards/zsa/voyager/keymaps/manna-harbour_miryoku/custom_config.h
+++ keyboards/zsa/voyager/keymaps/manna-harbour_miryoku/custom_config.h
@@ -5,36 +5,23 @@
 
 #pragma once
 
-#define XXX KC_NO
+/* #undef COMBO_COUNT */
+/* #define COMBO_COUNT 19 */
 
-#include "custom_config.h"
+#define CAPS_LOCK_STATUS
 
-#if defined (MIRYOKU_MAPPING_SHIFTED_ROWS)
-#define LAYOUT_miryoku( \
+#define RGBLIGHT_SLEEP
+
+#define MIRYOKU_MAPPING( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 LAYOUT_voyager( \
-XXX, K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, XXX, \
-XXX, K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, XXX, \
-XXX, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, XXX, \
-XXX, XXX, XXX, K32, K33, K34,      K35, K36, K37, XXX, XXX, XXX, \
-                    XXX, XXX,      XXX, XXX \
-)
-#else
-#define LAYOUT_miryoku( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-LAYOUT_voyager( \
-XXX, XXX, XXX, XXX, XXX, XXX,      XXX, XXX, XXX, XXX, XXX, XXX, \
-XXX, K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, XXX, \
-XXX, K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, XXX, \
-XXX, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, XXX, \
+KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,      KC_6, KC_7, KC_8, KC_9, KC_0, TD(QK_REBOOT), \
+KC_TAB, K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, KC_BSPC, \
+LSFT_T(KC_ESC), K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, RSFT_T(KC_ENT), \
+KC_LEFT_GUI, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, KC_RIGHT_CTRL, \
                     K33, K34,      K35, K36 \
 )
-#endif
